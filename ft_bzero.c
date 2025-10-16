c15508d3595db2713f4edcde416d23c6a17b64e7
Nicole Wirtzbiki <nwirtzbi@c1r8s8.42porto.com>
2025-10-16 18:15:40 +0100

    header and norminette ft_bezero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,11 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nwirtzbi <nwirtzbi@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 17:39:52 by nwirtzbi          #+#    #+#             */
+/*   Updated: 2025/10/16 18:12:57 by nwirtzbi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
 	unsigned char	*ptr;
-	size_t	i;
-	
+	size_t			i;
+
 	ptr = (unsigned char *)s;
 	i = 0;
 	while (i < n)
--- mains/main_bzero.c
+++ mains/main_bzero.c
@@ -1,5 +1,3 @@
-
-
 #include "libft.h"
 #include <string.h>
 #include <stdio.h>
@@ -9,16 +7,24 @@ int	main(void)
 	char str1[] = "Hello World!";
 	char str2[] = "Hello World!";
 
+	printf("Antes str1: %s\n", str1);
+	printf("Antes str2: %s\n", str2);
+
 	ft_bzero(str1 + 5, 3); //usando o ft_bzero
 	bzero(str2 + 5, 3); //usando o bzero da libc para comparar
 
-	printf("ft_bzero: %s\n", str1);
-	printf("bzero: %s\n", str2);
+	printf("Depois str1 com ft_bzero: %s\n", str1);
+	printf("Depois str2 com bzero: %s\n", str2);
 
-	//verificando os bytes manualmente
+	printf("Verificando bytes da str1:\n");
 	for (int i = 0; i < 12; i++)
-		printf("%02x ", (unsigned char)str1[i]);
-	printf("\n");
+	{
+		unsigned char c = (unsigned char)str1[i];
+		if (c == '\0')
+			printf("[%2d] 00 (.)\n", i);
+		else
+			printf("[%2d] %02x (%c)\n", i, c, c);
+	}
 
 	return (0);
 }
